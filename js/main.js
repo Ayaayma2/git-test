73bf607da5e2544496c7db5346cbd0e3a60db8eb
ayaayman <154886412+Ayaayma2@users.noreply.github.com>
2024-11-27 20:17:14 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,38 +1,105 @@
-var Quotes = [
-    {
-        qoutes: "“So many books, so little time.”",
-        author: "― Frank Zappa"
-    },
-    {
-        qoutes: "“Be yourself; everyone else is already taken.”",
-        author: "― Oscar Wilde"
-    },
-    {
-        qoutes: "“Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.”",
-        author: "― Bernard M. Baruch"
-    },
-    {
-        qoutes: "“You know you're in love when you can't fall asleep because reality is finally better than your dreams.”",
-        author: "― Dr. Seuss"
-    },
-    {
-        qoutes: "“In three words I can sum up everything I've learned about life: it goes on.”",
-        author: "― Robert Frost"
-    },
-    {
-        qoutes: "“If you want to know what a man's like, take a good look at how he treats his inferiors, not his equals.”",
-        author: "― J.K. Rowling, Harry Potter and the Goblet of Fire"
-    },
-    {
-        qoutes: "“I've learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.”",
-        author: "― Maya Angelou"
+var siteName_input = document.getElementById("sitename")
+var siteUrl_input = document.getElementById("siteurl")
+var sitesearch_input = document.getElementById("sitesearch")
+var mainindex = 0;
+var siteList;
+if (localStorage.getItem("LIST") == null) {
+    siteList = [];
+}
+else {
+    siteList = JSON.parse(localStorage.getItem("LIST"))
+    display()
+}
+
+function addSite() {
+    var site = {
+        name: siteName_input.value,
+        url: siteUrl_input.value
+    }
+    if (document.getElementById("submitsite").innerHTML == "Submit") {
+        siteList.push(site);
+    }
+    else {
+        siteList.splice(mainindex, 1, site)
+        document.getElementById("submitsite").innerHTML = "Submit"
     }
 
-]
 
-function fun_qoute() {
-    var random_number = Math.floor(Math.random() * Quotes.length)
-    document.getElementById("text-qoute").innerHTML = Quotes[random_number].qoutes;
-    document.getElementById("text-author").innerHTML = Quotes[random_number].author;
+    localStorage.setItem("LIST", JSON.stringify(siteList))
+    display()
+    clear()
+}
+
+function clear(){
+    siteName_input.value = ""
+    siteUrl_input.value = ""
+}
+function deletesite(x) {
+    siteList.splice(x, 1)
+    localStorage.setItem("LIST", JSON.stringify(siteList))
+    display();
+}
+function updatesite(siteIndex) {
+    mainindex = siteIndex
+    var site = siteList[siteIndex]
+    siteName_input.value = site.name
+    siteUrl_input.value = site.url
+    document.getElementById("submitsite").innerHTML = "Update"
 
 }
+
+function display() {
+    var temp = ""
+    for (var i = 1; i <= siteList.length; i++) {
+        temp += `<tr>
+                        <td> `+ i + `</td>
+                        <td>  `+siteList[i-1].name + `</td>
+                        <td><a href="`+ siteList[i-1].url + `" target="_blank"><button class="btn btn-warning">
+                                <i class="fa-solid fa-eye me-2"></i>
+                                visit</button></a>
+                                </td>
+                        <td><button onclick=deletesite(`+ (i-1) + `) " class="btn btn-danger">
+                        <i class="fa-solid fa-trash-can me-2"></i>
+                        delete</button></td>
+                         <td><button onclick=" updatesite(`+ (i-1)  + `)" class="btn btn-success">
+                                <i class="fa-solid fa-pen me-2"></i>
+                                update</button></td>
+
+                    </tr> `
+
+    }
+    document.getElementById("myTable").innerHTML = temp
+}
+
+
+
+function search() {
+    var searchvaL = sitesearch_input.value.toLowerCase()
+    var temp = ""
+    for (var i = 1; i <= siteList.length; i++) {
+        if (siteList[i-1].name.toLowerCase().includes(searchvaL) == true) {
+            temp += `<tr>
+            <td> `+ i + `</td>
+            <td>  `+ siteList[i-1].name + `</td>
+                        <td><a href="`+ siteList[i-1].url + `" target="_blank"><button class="btn btn-warning">
+                                <i class="fa-solid fa-eye me-2"></i>
+                                visit</button></a>
+                                </td>
+            <td><button onclick=deletesite(`+ (i-1) + `) " class="btn btn-danger">
+            <i class="fa-solid fa-trash-can me-2"></i>
+            delete</button></td>
+             <td><button class="btn btn-success">
+                                <i class="fa-solid fa-pen me-2"></i>
+                                update</button></td>
+
+        </tr> `
+
+        }
+
+    }
+    document.getElementById("myTable").innerHTML = temp
+}
+
+
+
+
