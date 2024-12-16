1a2e0b282f126291ba4091f1326c49e2478d1690
ayaayman <154886412+Ayaayma2@users.noreply.github.com>
2024-12-16 23:03:24 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,105 +1,234 @@
-var siteName_input = document.getElementById("sitename")
-var siteUrl_input = document.getElementById("siteurl")
-var sitesearch_input = document.getElementById("sitesearch")
-var mainindex = 0;
-var siteList;
+
+
+var finput_email = document.getElementById("femail_input")
+var finput_pass = document.getElementById("fpassword_input")
+var fp_incorrect = document.getElementById("fincorrect")
+var fp_incorrectinput = document.getElementById("finput_incorrect")
+
+
+var sinput_name = document.getElementById("sname_input")
+var psname_valid = document.getElementById("namevalid")
+var sinput_email = document.getElementById("semail_input")
+var psemail_valid = document.getElementById("email_valid")
+var sinput_pass = document.getElementById("spassword_input")
+var pspass_valid = document.getElementById("pass_valid")
+var sp_incorrect = document.getElementById("sincorrect")
+var sp_success = document.getElementById("success")
+
+var bow=document.getElementById("bow")
+
+var listperson
 if (localStorage.getItem("LIST") == null) {
-    siteList = [];
+    listperson = [];
 }
 else {
-    siteList = JSON.parse(localStorage.getItem("LIST"))
-    display()
+    listperson = JSON.parse(localStorage.getItem("LIST"))
+  if(bow!=null){
+    for(var i=0;i<listperson.length;i++){
+        if(JSON.parse(localStorage.getItem("userlogged"))==listperson[i].email){
+            bow.innerHTML+=" "+listperson[i].name
+        }
+    }
+  }
+    
 }
 
-function addSite() {
-    var site = {
-        name: siteName_input.value,
-        url: siteUrl_input.value
-    }
-    if (document.getElementById("submitsite").innerHTML == "Submit") {
-        siteList.push(site);
+function login() {
+    if (finput_email.value == "" || finput_pass.value == "") {
+        fp_incorrect.classList.remove("d-none")
     }
     else {
-        siteList.splice(mainindex, 1, site)
-        document.getElementById("submitsite").innerHTML = "Submit"
+        fp_incorrect.classList.add("d-none")
     }
 
-
-    localStorage.setItem("LIST", JSON.stringify(siteList))
-    display()
+    if (localStorage.getItem("LIST") != null) {
+        listperson = JSON.parse(localStorage.getItem("LIST"))
+        for (var i = 0; i < listperson.length; i++) {
+            if (listperson[i].email == finput_email.value && listperson[i].pass == finput_pass.value) {
+                window.location.href = "home.html"
+                localStorage.setItem("userlogged",JSON.stringify(listperson[i].email))
+            }
+            else{
+                fp_incorrectinput.classList.remove("d-none")
+            }
+        }
+        
+    }
     clear()
 }
 
-function clear(){
-    siteName_input.value = ""
-    siteUrl_input.value = ""
-}
-function deletesite(x) {
-    siteList.splice(x, 1)
-    localStorage.setItem("LIST", JSON.stringify(siteList))
-    display();
-}
-function updatesite(siteIndex) {
-    mainindex = siteIndex
-    var site = siteList[siteIndex]
-    siteName_input.value = site.name
-    siteUrl_input.value = site.url
-    document.getElementById("submitsite").innerHTML = "Update"
-
-}
-
-function display() {
-    var temp = ""
-    for (var i = 1; i <= siteList.length; i++) {
-        temp += `<tr>
-                        <td> `+ i + `</td>
-                        <td>  `+siteList[i-1].name + `</td>
-                        <td><a href="`+ siteList[i-1].url + `" target="_blank"><button class="btn btn-warning">
-                                <i class="fa-solid fa-eye me-2"></i>
-                                visit</button></a>
-                                </td>
-                        <td><button onclick=deletesite(`+ (i-1) + `) " class="btn btn-danger">
-                        <i class="fa-solid fa-trash-can me-2"></i>
-                        delete</button></td>
-                         <td><button onclick=" updatesite(`+ (i-1)  + `)" class="btn btn-success">
-                                <i class="fa-solid fa-pen me-2"></i>
-                                update</button></td>
-
-                    </tr> `
-
+function fvaildemail() {
+    var regexemail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+    if (regexemail.test(finput_email.value) == true) {
+        fp_incorrectinput.classList.add("d-none")
+        finput_email.classList.add("is-valid")
+        finput_email.classList.remove("is-invalid")
+        fp_incorrect.classList.add("d-none")
+        return true
     }
-    document.getElementById("myTable").innerHTML = temp
+    else {
+        fp_incorrectinput.classList.remove("d-none")
+        finput_email.classList.remove("is-valid")
+        finput_email.classList.add("is-invalid")
+        fp_incorrect.classList.add("d-none")
+        return false
+    }
+
 }
+finput_email.addEventListener("change", fvaildemail)
 
+function fvaildpassword() {
+    var regexpass = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+    if (regexpass.test(finput_pass.value) == true) {
+        fp_incorrectinput.classList.add("d-none")
+        finput_pass.classList.add("is-valid")
+        finput_pass.classList.remove("is-invalid")
+        fp_incorrect.classList.add("d-none")
+        return true
+    }
+    else {
+        fp_incorrectinput.classList.remove("d-none")
+        finput_pass.classList.remove("is-valid")
+        finput_pass.classList.add("is-invalid")
+        fp_incorrect.classList.add("d-none")
+        return false
+    }
+}
+finput_pass.addEventListener("change", fvaildpassword)
 
+// -----------------------------------------------
 
-function search() {
-    var searchvaL = sitesearch_input.value.toLowerCase()
-    var temp = ""
-    for (var i = 1; i <= siteList.length; i++) {
-        if (siteList[i-1].name.toLowerCase().includes(searchvaL) == true) {
-            temp += `<tr>
-            <td> `+ i + `</td>
-            <td>  `+ siteList[i-1].name + `</td>
-                        <td><a href="`+ siteList[i-1].url + `" target="_blank"><button class="btn btn-warning">
-                                <i class="fa-solid fa-eye me-2"></i>
-                                visit</button></a>
-                                </td>
-            <td><button onclick=deletesite(`+ (i-1) + `) " class="btn btn-danger">
-            <i class="fa-solid fa-trash-can me-2"></i>
-            delete</button></td>
-             <td><button class="btn btn-success">
-                                <i class="fa-solid fa-pen me-2"></i>
-                                update</button></td>
+var emailfound=document.getElementById("emailfound")
 
-        </tr> `
+function signup() {
+    if (sinput_email.value == "" || sinput_pass.value == "" || sinput_name.value == "") {
+        sp_incorrect.classList.remove("d-none")
+        sp_success.classList.add("d-none")
+    }
+    else {
+        sp_incorrect.classList.add("d-none")
+        sp_success.classList.remove("d-none")
+    }
 
+    if (svaildname() == true && svaildemail() == true && svaildpassword() == true) {
+        var person = {
+            name: sinput_name.value,
+            email: sinput_email.value,
+            pass: sinput_pass.value
         }
 
     }
-    document.getElementById("myTable").innerHTML = temp
+
+        if (localStorage.getItem("LIST") == null) {
+            listperson = [];
+        }
+        else {
+            listperson = JSON.parse(localStorage.getItem("LIST"))
+        }
+            
+        for (var i = 0; i < listperson.length; i++) {
+            if (listperson[i].email == sinput_email.value) {
+                emailfound.classList.remove("d-none")
+                sp_success.classList.add("d-none")
+                return; 
+            }
+        }
+        listperson.push(person);
+        emailfound.classList.add("d-none")
+        sp_success.classList.remove("d-none")
+        localStorage.setItem("LIST", JSON.stringify(listperson))
+
+    clear()
+}
+
+function svaildname() {
+    var regexname = /^[a-zA-Z]{2,30}$/
+    if (regexname.test(sinput_name.value) == true) {
+        psname_valid.classList.add("d-none")
+        sinput_name.classList.add("is-valid")
+        sinput_name.classList.remove("is-invalid")
+        sp_incorrect.classList.add("d-none")
+        return true
+    }
+    else {
+        psname_valid.classList.remove("d-none")
+        sinput_name.classList.remove("is-valid")
+        sinput_name.classList.add("is-invalid")
+        sp_incorrect.classList.add("d-none")
+        return false
+    }
+}
+sinput_name.addEventListener("change", svaildname)
+
+function svaildemail() {
+    var regexemail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+    if (regexemail.test(sinput_email.value) == true) {
+        psemail_valid.classList.add("d-none")
+        sinput_email.classList.add("is-valid")
+        sinput_email.classList.remove("is-invalid")
+        sp_incorrect.classList.add("d-none")
+        return true
+    }
+    else {
+        psemail_valid.classList.remove("d-none")
+        sinput_email.classList.remove("is-valid")
+        sinput_email.classList.add("is-invalid")
+        sp_incorrect.classList.add("d-none")
+        return false
+    }
+}
+sinput_email.addEventListener("change", svaildemail)
+
+function svaildpassword() {
+    var regexpass = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+    if (regexpass.test(sinput_pass.value) == true) {
+        pspass_valid.classList.add("d-none")
+        sinput_pass.classList.add("is-valid")
+        sinput_pass.classList.remove("is-invalid")
+        sp_incorrect.classList.add("d-none")
+        return true
+    }
+    else {
+        pspass_valid.classList.remove("d-none")
+        sinput_pass.classList.remove("is-valid")
+        sinput_pass.classList.add("is-invalid")
+        sp_incorrect.classList.add("d-none")
+        return false
+    }
+}
+sinput_pass.addEventListener("change", svaildpassword)
+
+function clear() {
+    finput_email.value = ""
+    finput_pass.value = ""
+    sinput_name.value = ""
+    sinput_email.value = ""
+    sinput_pass.value = ""
+    finput_email.classList.remove("is-valid")
+        finput_email.classList.remove("is-invalid")
+        finput_pass.classList.remove("is-valid")
+        finput_pass.classList.remove("is-invalid")
+    sinput_name.classList.remove("is-valid")
+    sinput_name.classList.remove("is-invalid")
+    sinput_email.classList.remove("is-valid")
+    sinput_email.classList.remove("is-invalid")
+    sinput_pass.classList.remove("is-valid")
+    sinput_pass.classList.remove("is-invalid")
+    psname_valid.classList.add("d-none")
+    psemail_valid.classList.add("d-none")
+    pspass_valid.classList.add("d-none")
+    
 }
 
 
 
+function logout(){
+     window.location.href ="index.html"
+    for(var i=0;i<listperson.length;i++){
+        listperson.splice(listperson[i],1)
+        localStorage.setItem("LIST",JSON.stringify(listperson))
+    }
+   
+    clear()
 
+}
